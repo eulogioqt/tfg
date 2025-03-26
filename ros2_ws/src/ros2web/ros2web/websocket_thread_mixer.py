c5163481554e05bddbf7ec252db732d9bed2bd1e
Eulogio Quemada <elgameshdpay@gmail.com>
2025-03-26 12:11:48 +0100

    Create websocket_thread_mixer.py

---
+++ ros2_ws/src/ros2web/ros2web/websocket_thread_mixer.py
@@ -0,0 +1,28 @@
+import threading
+
+class WebSocketThreadMixer:
+
+    def __init__(self, websocket, *others):
+        self.websocket = websocket
+        self.others = others 
+        self.threads = {}
+
+    def run(self):
+        for other in self.others: # Arrancamos hilos
+            thread = threading.Thread(target=other.run, daemon=False)
+            thread.start()
+
+            self.threads[other.__class__.__name__] = thread
+            
+        try: self.websocket.run() # Principal websockets
+        finally:
+            print("WebSocketServer finalizado.")
+
+            for other in self.others: # Paramos hilos
+                other.stop()
+
+            for th_name in self.threads.keys(): # Esperamos su muerte
+                self.threads[th_name].join()
+                print(th_name + " finalizado.")
+
+            print("Programa terminado.")
