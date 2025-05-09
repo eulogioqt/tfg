b9fbb63a12a663b88c28d0a37f536f09cb7d9332
Eulogio Quemada <elgameshdpay@gmail.com>
2025-05-09 13:56:43 +0200

    WhisperSTT

---
+++ ros2_ws/src/speech_tools/speech_tools/stt/__init__.py
@@ -0,0 +1,3 @@
+from .stt_model import STTModel
+from .google_stt import GoogleSTT
+from .whisper_stt import WhisperSTT
--- ros2_ws/src/speech_tools/speech_tools/stt/whisper_stt.py
+++ ros2_ws/src/speech_tools/speech_tools/stt/whisper_stt.py
@@ -1,1 +1,35 @@
+import os
+import torch
+import torchaudio
+import tempfile
+
+from faster_whisper import WhisperModel
+
+from .stt_model import STTModel
+
+
 # a futuro hacer faster whisper y whisper como difernetes, si eso meter mas STT y poner lo de provider y model como en los llms
+class WhisperSTT(STTModel):
+    def __init__(self, model_size: str = "base", device: str = "cuda", compute_type: str = "float16"):
+        self.model = WhisperModel(model_size, device=device, compute_type=compute_type)
+
+    def transcribe(self, audio: bytes, sample_rate: int) -> str:
+        try:
+            audio_tensor = torch.tensor(torch.frombuffer(audio, dtype=torch.int16).float() / 32768.0).unsqueeze(0)
+
+            with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as tmpfile:
+                tmp_path = tmpfile.name
+                torchaudio.save(tmp_path, audio_tensor, sample_rate)
+
+            segments, _ = self.model.transcribe(tmp_path, language="es")
+            text = " ".join([segment.text for segment in segments])
+
+            return text
+
+        except Exception as e:
+            print(f"[WhisperSTT] Error: {e}")
+            return ""
+
+        finally:
+            if 'tmp_path' in locals() and os.path.exists(tmp_path):
+                os.remove(tmp_path)
