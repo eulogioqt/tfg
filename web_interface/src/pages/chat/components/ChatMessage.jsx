21ad5a244e3385097c30fc91ded9098cd97a6383
Eulogio Quemada <euquemada@gmail.com>
2025-05-12 23:50:29 +0200

    Update ChatMessage

--- web_interface/src/pages/chat/components/ChatMessage.jsx
+++ web_interface/src/pages/chat/components/ChatMessage.jsx
@@ -1,21 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useChat } from "../../../contexts/ChatContext";
 
 const ChatMessage = ({ message }) => {
     const { playAudio } = useChat();
+
+    const [timer, setTimer] = useState(0);
+
     const isHuman = message.isHuman;
     const backgroundColor = isHuman ? "#E0E0E0" : "#FFFFFF";
     const shadowColor = isHuman ? "#C0C0C0" : "#DDDDDD";
 
+    useEffect(() => {
+        let intervalId;
+        if (message.value) {
+            if (timer > 0) setTimer(0);
+        } else {
+            intervalId = setInterval(() => {
+                setTimer((prev) => prev + 100);
+            }, 100);
+        }
+
+        return () => clearInterval(intervalId);
+    }, [message.value]);
+
+    const getMessageFooter = (isHuman, data, timestamp) => {
+        return (
+            <div className={"d-flex justify-content-between " + (isHuman ? "" : "flex-row-reverse")}>
+                <div>
+                    <span className="px-0 py-0 badge text-muted">{timestamp}</span>
+                </div>
+
+                <div>
+                    {data.map(
+                        (item) =>
+                            item.value && (
+                                <span className={"px-0 py-0 badge text-muted " + (isHuman ? "ms-2" : "me-2")}>
+                                    <i className={`bi bi-${item.icon} me-1`}></i>
+                                    {item.value}
+                                </span>
+                            )
+                    )}
+                </div>
+            </div>
+        );
+    };
+
     return (
-        <div className={`d-flex ${isHuman ? "justify-content-end" : "justify-content-start"}`}>
-            {/* Tema audio */}
+        <div className={`d-flex ${isHuman ? "flex-row-reverse" : ""}`}>
+            {/* Boton reproducir audio ¿Pongo como whatsapp que sale las barritas de intensidad? */}
             {message.audio && message.sampleRate && (
                 <div className="d-flex flex-column align-items-center justify-content-center mb-3">
                     <i
                         onClick={() => playAudio(message.audio, message.sampleRate)}
-                        className="d-flex justify-content-center align-items-center bi bi-play bg-dark text-white me-1 rounded-circle border"
-                        style={{ width: "48px", height: "48px", cursor: "pointer" }}
+                        title="Reproducir..."
+                        className={`d-flex justify-content-center align-items-center bi bi-play bg-dark text-white rounded-circle border ${
+                            isHuman ? "ms-2" : "me-2"
+                        }`}
+                        style={{ fontSize: "1.5em", width: "48px", height: "48px", cursor: "pointer" }}
                     ></i>
                 </div>
             )}
@@ -29,49 +70,37 @@ const ChatMessage = ({ message }) => {
                     maxWidth: "70%",
                     padding: "4px 10px",
                     wordWrap: "break-word",
-                    lineHeight: "1rem",
+                    lineHeight: "1.25em",
                 }}
             >
                 {/* Contenido del mensaje */}
-                <div className="d-flex flex-column align-items-start justify-content-between">
-                    <span className={`w-100 ${isHuman ? "text-end" : "text-start"}`}>{message.value}</span>
+                <div className="d-flex flex-column align-items-start justify-content-between text-justify">
+                    <span className="w-100 text-start">
+                        {message.value || "Transcribiendo... (" + (timer / 1000).toFixed(1) + "s)"}
+                    </span>
                 </div>
 
                 {/* Info humanos */}
-                {isHuman && (message.intent || message.sttModel) && (
-                    <div className="text-end w-100 mt-1">
-                        {message.sttModel && (
-                            <span className="pe-0 py-0 badge text-muted text-end ms-1">
-                                <i className="bi bi-mic me-1"></i>
-                                {message.sttModel}
-                            </span>
-                        )}
-                        {message.intent && (
-                            <span className="pe-0 py-0 badge text-muted text-end ms-1">
-                                <i className="bi bi-robot me-1"></i>
-                                {message.intent}
-                            </span>
-                        )}
-                    </div>
-                )}
+                {isHuman &&
+                    getMessageFooter(
+                        true,
+                        [
+                            { value: message.sttModel, icon: "mic" },
+                            { value: message.intent, icon: "compass" },
+                        ],
+                        message.timestamp
+                    )}
 
                 {/* Info bot */}
-                {!isHuman && (message.provider || message.ttsModel) && (
-                    <div className="mt-1">
-                        {message.provider && (
-                            <span className="ps-0 py-0 badge text-muted me-1">
-                                <i className="bi bi-robot me-1"></i>
-                                {message.provider}
-                            </span>
-                        )}
-                        {message.ttsModel && (
-                            <span className="ps-0 py-0 badge text-muted me-1">
-                                <i className="bi bi-volume-up me-1"></i>
-                                {message.ttsModel}
-                            </span>
-                        )}
-                    </div>
-                )}
+                {!isHuman &&
+                    getMessageFooter(
+                        false,
+                        [
+                            { value: message.provider, icon: "robot" },
+                            { value: message.ttsModel, icon: "volume-up" },
+                        ],
+                        message.timestamp
+                    )}
             </div>
         </div>
     );
