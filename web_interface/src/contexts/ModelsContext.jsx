94febda776240e27046fb91fd6317e54a1a656c2
Eulogio Quemada <euquemada@gmail.com>
2025-05-13 12:31:41 +0200

    Fix

--- web_interface/src/contexts/ModelsContext.jsx
+++ web_interface/src/contexts/ModelsContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useRef, useState } from "react";
 import { useToast } from "./ToastContext";
 import { useAPI } from "./APIContext";
 import { useWebSocket } from "./WebSocketContext";
