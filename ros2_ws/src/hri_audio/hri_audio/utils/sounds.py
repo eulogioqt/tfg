168dacc3c95005488914045e59b1004651e8c612
Antonio Cañete Baena <antoniocanetebaena1234@gmail.com>
2025-05-16 12:55:28 +0200

    Final fix

--- ros2_ws/src/hri_audio/hri_audio/utils/sounds.py
+++ ros2_ws/src/hri_audio/hri_audio/utils/sounds.py
@@ -2,7 +2,7 @@ import os
 from .sound import load
 
 activation_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "sounds/activation_sound.wav")
-time_out_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "sounds/time_out_path.wav")
+time_out_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "sounds/time_out_sound.wav")
 
 ACTIVATION_SOUND = load(activation_path)
 TIME_OUT_SOUND = load(time_out_path)
--- ros2_ws/src/hri_audio/setup.py
+++ ros2_ws/src/hri_audio/setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='mapir',
-    maintainer_email='famoreno@uma.es',
+    maintainer_email='eulogioquemada@uma.es',
     description='TODO: Package description',
     license='Apache-2.0',
     tests_require=['pytest'],
