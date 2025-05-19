30b39bbbd0208ebea2ff11f5d5ec4ea6ac1c562e
Eulogio Quemada <euquemada@gmail.com>
2025-05-19 20:39:47 +0200

    Silero VAD

---
+++ ros2_ws/src/hri_audio/hri_audio/utils/__init__.py
@@ -0,0 +1,6 @@
+from .model_hotword import ModelHotword
+
+from .pvporcupine_hotword import PVPorcupineHotword
+from .stt_hotword import STTHotword
+
+from .silero_vad import SileroVAD
--- ros2_ws/src/hri_audio/hri_audio/utils/gtranslate.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def translate(texto, idioma_origen="es", idioma_destino="en"):
-    url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl={}&tl={}&dt=t&q={}".format(idioma_origen, idioma_destino, texto)
-    response = requests.get(url)
-    traduccion = response.json()[0][0][0]
-    
-    return traduccion
