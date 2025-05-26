dd0274ffa17f4b009a8ef8596dbe922d4db8633b
Eulogio Quemada <euquemada@gmail.com>
2025-05-26 14:28:18 +0200

    Show photo done

--- ros2_ws/src/hri_audio/hri_audio/assistant_node.py
+++ ros2_ws/src/hri_audio/hri_audio/assistant_node.py
@@ -6,8 +6,10 @@ from rclpy.node import Node
 from queue import Queue
 
 from std_msgs.msg import String
-from hri_msgs.srv import SanchoPrompt
+from hri_msgs.srv import SanchoPrompt, TriggerUserInteraction
 from speech_msgs.srv import TTS
+
+from sancho_ai.sancho_ai.prompts.commands import COMMANDS
 
 
 class AssistantNode(Node):
@@ -25,6 +27,11 @@ class AssistantNode(Node):
         self.tts_client = self.create_client(TTS, 'speech_tools/tts')
         while not self.tts_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('TTS service not available, waiting again...')
+
+        self.gui_client = self.create_client(TriggerUserInteraction, 'gui/request')
+        while not self.gui_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('GUI service not available, waiting again...')
+        
 
         self.queue = Queue(maxsize=1)
         self.get_logger().info("Assistant Node initializated succesfully.")
@@ -44,17 +51,18 @@ class Assistant:
                 user_text = self.node.queue.get()
 
                 self.node.face_mode_pub.publish(String(data="thinking"))
-                ai_response, emotion, _ = self.sancho_prompt_request(user_text)
-
+                ai_response, emotion, data, intent = self.sancho_prompt_request(user_text)
                 self.node.get_logger().info(f"✅✅✅ Respuesta recibida '{ai_response}'")
+
+                if intent == COMMANDS.TAKE_PICTURE:
+                    self.gui_request("show_photo", data) # Show photo
 
                 self.node.face_mode_pub.publish(String(data="speaking"))
                 self.node.face_mode_pub.publish(String(data=emotion.lower()))
                 audio, sample_rate = self.tts_request(ai_response)
                 self.node.get_logger().info(f"✅✅✅ Reproduciendo por audio la respuesta")
 
-                sd.play(audio, samplerate=sample_rate)
-                sd.wait()
+                self.play_audio(audio, sample_rate)
                 self.node.face_mode_pub.publish(String(data="idle"))
 
             rclpy.spin_once(self.node)
@@ -74,7 +82,7 @@ class Assistant:
         emotion = value["emotion"]
         data = value["data"]
 
-        return text, emotion, data
+        return text, emotion, data, result_sancho_prompt.intent
 
     def tts_request(self, text):
         tts_request = TTS.Request()
@@ -85,6 +93,23 @@ class Assistant:
         result_tts = future_tts.result()
 
         return result_tts.audio, result_tts.sample_rate
+
+    def gui_request(self, mode, data_json):
+        req = TriggerUserInteraction.Request()
+        req.mode = mode
+        req.data_json = data_json
+
+        future = self.node.gui_client.call_async(req)
+        rclpy.spin_until_future_complete(self.node, future)
+        result = future.result()
+
+        return result.accepted
+
+    def play_audio(self, audio, sample_rate, wait=True):
+        sd.play(audio, samplerate=sample_rate)
+        if wait:
+            sd.wait()
+
 
 def main(args=None):
     rclpy.init(args=args)
